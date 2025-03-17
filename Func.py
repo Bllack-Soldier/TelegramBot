a5df42f79921f187d0f42370f0992512f58f195a
Black-Soldier <119063254+Bllack-Soldier@users.noreply.github.com>
2025-03-17 16:35:22 +0330

    Delete Func.py

--- Func.py
+++
@@ -1,7 +0,0 @@
-class Func():
-    
-    def get_all_usernames(user_dict):
-        usernames=[]
-        for user_identifier, user_data in user_dict.items():
-            usernames.append(user_data["username"])
-        return usernames
